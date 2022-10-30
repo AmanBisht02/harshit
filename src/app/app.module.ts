4eaf0520545397e2e56b30d503fe7dfebe5ca1a8
AmanBisht02 <amanbisht09876@gmail.com>
2022-10-30 21:39:28 +0530

    latest commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+// import { ForgotPasswordPage } from "./forgot-password/forgot-password.page";
+// import { LoginPage } from "./login/login.page";
+// import { RegistrationPage } from "./registration/registration.page";
+// import { HomePage } from "./home/home.page";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    // RegistrationPage,
+    // ForgotPasswordPage,
+    // LoginPage,
+    // HomePage,
+  ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
